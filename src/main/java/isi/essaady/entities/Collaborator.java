24770a9b9b438bce73a7e330ce335cb0858e77e4
Kuohamkun <50623506+Kuohamkun@users.noreply.github.com>
2020-10-25 16:35:07 +0100

    Commit the untracked Collaborator bean file

--- src/main/java/isi/essaady/entities/Collaborator.java
+++ src/main/java/isi/essaady/entities/Collaborator.java
@@ -57,6 +57,15 @@ public class Collaborator implements Serializable {
 	
 	public Collaborator() {
 	}
+	
+	public Collaborator(Collaborator collabToCopy) {
+		this.idCollab = collabToCopy.idCollab;
+		this.firstName = collabToCopy.firstName;
+		this.lastName = collabToCopy.lastName;
+		this.email = collabToCopy.email;
+		this.gender = collabToCopy.gender;
+	
+	}
 
 	public int getIdCollab() {
 		return this.idCollab;
@@ -136,4 +145,50 @@ public class Collaborator implements Serializable {
 		this.competences = competences;
 	}
 
+	/* HASHCODE - EQUALS */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + idCollab;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Collaborator other = (Collaborator) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (idCollab != other.idCollab)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	} 
 }
